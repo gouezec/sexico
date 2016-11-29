3fe77d847e79c480e3cc8aa393dbab4e2db6995d
to28077 <to28077@TOLL302000177.tls.fr.eu.airbus.corp>
2016-11-29 16:46:12 +0100

    Ajout de la conf MEXICO Medium

---
+++ SQLMexico/data/MediumConf/operateurs_mexico.h
@@ -0,0 +1,68 @@
+/******************************************************************/
+/*                     ssdb_op.h                                 */
+/******************************************************************/
+
+float absolu (float f);
+float deg2rad (float f);
+float div10 (float f);
+float div100(float f);
+float div1000 (float f);
+float div10000 (float f);
+float divG (float f);
+float drpal (float f);
+float drpal200 (float f);
+float freq2vitessA320 (float;f)
+float ft2m (float f);
+float gaz1 (float f);
+float gaz2 (float f);
+float identique (float f);
+int int2bool16 (int i);
+float kg2klbs (float f);
+float kg2lbs (float f);
+float klbs2kg (float f);
+float kt2ms (float f);
+float lbs2kg (float f);
+float m2ft (float f);
+float moins1 (float f);
+float ms2kt (float f);
+logical notn (logical l);
+logical notp (logical l);
+float oppose (float f);
+float rad2deg (float f);
+float range21to30 (float f);
+float refresh (float f);
+float round2dec (float f);
+float speed2freq (float f);
+float x10 (float f);
+float x100 (float f);
+float x1000(float f);
+float x2tiers (float f);
+float range21to30 (float f);
+float div50 (float f);
+float opPlus1 (float f);
+float xpd18to30 (float f);
+float sA2E (float f);
+float sE2A (float f);
+float FoisG (float f);
+float FoisOppG (float f);
+float psi2bar (float f);
+float bar2psi (float f);
+float op_ssm (float f);
+float op_rpm (float f);
+float opDC (float f);
+boolean convert_boolean (boolean b);
+float op16to18 (float f);
+float bar2kpa (float f);
+float lbph2kgps (float f);
+float kgps2lbph (float f);
+float val2fs (float f);
+float oppDeg2Rad (float f);
+float oppose_deg2rad (float f);
+float Kel2Cel (float f);
+float Cel2Kel (float f);
+float Fois2 (float f);
+float deg180 (float f);
+float deg360 (float f);
+float x10_3 (float f);
+float lineaireP15M30 (float f);
+float lineaireP25M25 (float f);
